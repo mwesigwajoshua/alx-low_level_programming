9e84f25a1539267f0e40fe40881e61c8e1acf0ed
mwesigwajoshua <mwesigwajoshua23@gmail.com>
2023-07-11 07:44:45 -0700

    ma

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,50 +1,80 @@
 #include "main.h"
 #include <stdlib.h>
-
 /**
- * strtow - A function that splits a string into words
- * @str: An input pointer of the string to split
- * Return: Apointer to concatened strings or NULL if it str is NULL
+ * argstostr - concatenates arguments.
+ * @ac: number of string to be concatenated.
+ * @av: strings to be concatenated.
+ * Return: String concatenated.
  */
-char **strtow(char *str)
+char *argstostr(int ac, char **av)
 {
-	char **array;
-	int i = 0, j, m, k = 0, len = 0, count = 0;
+	int i, j, k = 0;
+	char *concat = NULL;
 
-	if (str == NULL || *str == '\0')
+	if (ac == 0 || av == NULL)
 		return (NULL);
-	for (; str[i]; i++)
+	for (i = 0; i < ac; i++)
+		for (j = 0; av[i][j] != '\0'; j++)
+			k++;
+	concat = malloc(sizeof(char) * (k + ac + 1));
+	if (concat == NULL)
+		return (NULL);
+	for (i = 0, k = 0; i < ac; i++)
 	{
-		if ((str[i] != ' ' || *str != '\t') &&
-				((str[i + 1] == ' ' || str[i + 1] == '\t') || str[i + 1] == '\n'))
-			count++;
+		for (j = 0; av[i][j] != '\0'; j++, k++)
+			concat[k] = av[i][j];
+		concat[k] = '\n';
+		k++;
 	}
-	if (count == 0)
-		return (NULL);
-	array = malloc(sizeof(char *) * (count + 1));
-	if (array == NULL)
-		return (NULL);
-	for (i = 0; str[i] != '\0' && k < count; i++)
-	{
-		if (str[i] != ' ' || str[i] != '\t')
-		{
-			len = 0;
-			j = i;
-			while ((str[j] != ' ' || str[j] != '\t') && str[j] != '\0')
-				j++, len++;
-			array[k] = malloc((len + 1) * sizeof(char));
-			if (array[k] == NULL)
-			{
-				for (k = k - 1; k >= 0; k++)
-					free(array[k]);
-				free(array);
-				return (NULL);
-			}
-			for (m = 0; m < len; m++, i++)
-				array[k][m] = str[i];
-			array[k++][m] = '\0';
-		}
-	}
-	array[k] = NULL;
-	return (array);
+	concat[k] = '\0';
+	return (concat);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
